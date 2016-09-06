7d292c2aee37afc4f81c90f2edce572f26abdf7b
Ly Nguyen <Ly900@aol.com>
2016-09-05 23:34:22 -0400

    Clicking on button increments count variable and updates in frame.

--- SimpleGui1.java
+++ SimpleGui1.java
@@ -1,18 +1,31 @@
 import javax.swing.*;
+import java.awt.event.*;
 
-public class SimpleGui1 {
+public class SimpleGui1 implements ActionListener {
+  JButton button;
+  public static int count;
 
   public static void main(String[] args) {
+    SimpleGui1 gui = new SimpleGui1();
+    gui.go();
+  }
 
+  public void go() {
+    count = 0;
     JFrame frame = new JFrame();
-    JButton button = new JButton("click me");
+    button = new JButton("click me " + count);
 
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    button.addActionListener(this);
 
     frame.getContentPane().add(button);
     frame.setSize(300, 300);
     frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
 
+  public void actionPerformed(ActionEvent event) {
+    count++;
+    button.setText(Integer.toString(count));
   }
 
 }
